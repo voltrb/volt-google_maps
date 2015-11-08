cfd6f236bee78559e3b36678327583fbcf6c9163
Ryan Stout <ryanstout@gmail.com>
2015-11-08 14:30:15 -0700

    add option to skip include'

--- app/google_maps/config/dependencies.rb
+++ app/google_maps/config/dependencies.rb
@@ -1,4 +1,4 @@
 # Component dependencies
-if Volt.config.public.google_maps_api_key
+if Volt.config.public.google_maps_api_key && !Volt.config.google_maps_skip_js_file
   javascript_file "https://maps.googleapis.com/maps/api/js?key=" + Volt.config.public.google_maps_api_key
 end
